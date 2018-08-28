52ee05d135c069a2f152832b0828356585d650ed
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2018-08-28 10:16:50 +0200

    esClient creation

--- vectortile-server/src/main/java/org/gbif/maps/TileServerApplication.java
+++ vectortile-server/src/main/java/org/gbif/maps/TileServerApplication.java
@@ -2,11 +2,16 @@ package org.gbif.maps;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.util.ContextInitializer;
+import com.google.common.base.Preconditions;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 import org.gbif.maps.common.meta.MapMetastore;
 import org.gbif.maps.common.meta.Metastores;
 import org.gbif.maps.resource.*;
+import org.gbif.occurrence.search.es.EsConfig;
 import org.gbif.occurrence.search.heatmap.OccurrenceHeatmapService;
 import org.gbif.occurrence.search.heatmap.es.EsOccurrenceHeatmapResponse;
+import org.gbif.occurrence.search.heatmap.es.OccurrenceHeatmapsEsService;
 import org.gbif.ws.discovery.lifecycle.DiscoveryLifeCycle;
 
 import io.dropwizard.Application;
@@ -16,6 +21,10 @@ import io.dropwizard.setup.Environment;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
 
 /**
  * The main entry point for running the member node.
@@ -50,36 +59,36 @@ public class TileServerApplication extends Application<TileServerConfiguration> 
     Configuration conf = HBaseConfiguration.create();
     conf.set("hbase.zookeeper.quorum", configuration.getHbase().getZookeeperQuorum());
 
-    //TODO: create instance using the elastic search client
-    OccurrenceHeatmapService<EsOccurrenceHeatmapResponse> heatmapsService = null;
-
+    EsConfig esConfig = configuration.getEsConfig().getEsConfig();
+    RestClient esClient = createEsClient(esConfig);
+    OccurrenceHeatmapService<EsOccurrenceHeatmapResponse> heatmapsService = new OccurrenceHeatmapsEsService(esClient, esConfig.getIndex());
 
     // Either use Zookeeper or static config to locate tables
     HBaseMaps hbaseMaps = null;
     if (configuration.getMetastore() != null) {
       MapMetastore meta = Metastores.newZookeeperMapsMeta(configuration.getMetastore().getZookeeperQuorum(), 1000,
-                                                  configuration.getMetastore().getPath());
+        configuration.getMetastore().getPath());
       hbaseMaps = new HBaseMaps(conf, meta, configuration.getHbase().getSaltModulus());
 
     } else {
       //
       MapMetastore meta = Metastores.newStaticMapsMeta(configuration.getHbase().getTilesTableName(),
-                                                       configuration.getHbase().getPointsTableName());
+        configuration.getHbase().getPointsTableName());
       hbaseMaps = new HBaseMaps(conf, meta, configuration.getHbase().getSaltModulus());
     }
 
 
     TileResource tiles = new TileResource(conf,
-                                          hbaseMaps,
-                                          configuration.getHbase().getTileSize(),
-                                          configuration.getHbase().getBufferSize());
+      hbaseMaps,
+      configuration.getHbase().getTileSize(),
+      configuration.getHbase().getBufferSize());
     environment.jersey().register(tiles);
 
 
     // The resource that queries ES directly for HeatMap data
     environment.jersey().register(new EsResource(heatmapsService,
-                                  configuration.getEsConfig().getTileSize(),
-                                  configuration.getEsConfig().getBufferSize()));
+      configuration.getEsConfig().getTileSize(),
+      configuration.getEsConfig().getBufferSize()));
 
     environment.jersey().register(new BackwardCompatibility(tiles));
 
@@ -88,5 +97,24 @@ public class TileServerApplication extends Application<TileServerConfiguration> 
     if (configuration.getService().isDiscoverable()) {
       environment.lifecycle().manage(new DiscoveryLifeCycle(configuration.getService()));
     }
+  }
+
+  private RestClient createEsClient(EsConfig esConfig) {
+    Objects.requireNonNull(esConfig);
+    Objects.requireNonNull(esConfig.getHosts());
+    Preconditions.checkArgument(esConfig.getHosts().length > 0);
+
+    HttpHost[] hosts = new HttpHost[esConfig.getHosts().length];
+    int i = 0;
+    for (String host : esConfig.getHosts()) {
+      try {
+        URL url = new URL(host);
+        hosts[i] = new HttpHost(url.getHost(), url.getPort(), url.getProtocol());
+        i++;
+      } catch (MalformedURLException e) {
+        throw new IllegalArgumentException(e.getMessage(), e);
+      }
+    }
+    return RestClient.builder(hosts).build();
   }
 }
